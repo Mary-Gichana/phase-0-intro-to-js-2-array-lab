ed9ac00f0fd159c52f8d44ed62451ec5ce5330f1
Mary Monchari Gichana <marygichana95@gmail.com>
2024-09-27 17:23:49 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name = "Ralph") {
+  cats.push(name);
+}
+function destructivelyPrependCat(name = "Bob"){
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name = "Garfield"){
+  cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name = "Milo"){
+  cats.shift(name);
+}
+function appendCat(name = "Broom"){
+   return ([...cats, name]);
+}
+function prependCat(name = "Arnold"){
+   return([name ,...cats]);
+}
+function removeLastCat( ){
+    return cats.slice(0, cats.length - 1);;
+}
+function removeFirstCat(){
+    return ([ ...cats.slice(1)]);
+}
+
